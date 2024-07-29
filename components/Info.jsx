ecbf52f16b8156938489ad9522cf3c243a3c252d
Levi Adissi <leviadissi@gmail.com>
2024-07-28 17:30:37 -0700

    Add files via upload

--- components/Info.jsx
+++ components/Info.jsx
@@ -4,7 +4,7 @@ import '../src/style.css'
 export default function Info() {
     return(
         <div className="info-container">
-            <img className="img-profile" src="/components/00_linkedinLevi.jpeg" />
+            <img className="img-profile" src="public/00_linkedinLevi.jpeg" />
             <h1 className="info-name">Levi Adissi</h1>
             <h2 className="info-title">Product Manager</h2>
             <h3 className="info-site"><a href="https://ladissi.github.io/">ladissi.github.io</a></h3>
