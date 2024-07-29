cfba6a0e3a6026e68ccc1cef12610bf5174b3d26
Levi Adissi <leviadissi@gmail.com>
2024-07-28 17:03:59 -0700

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+// import React from "react"
+// import TwitterIcon from "./Twitter Icon.jpg"
+import '../src/style.css'
+
+export default function Footer() {
+    return (
+        <div className="footer-container">
+            <p className="footer-p">NO COPYRIGHT.</p>
+        </div>
+    )
+}
---
+++ components/Interests.jsx
@@ -0,0 +1,12 @@
+// import React from "react"
+import '../src/style.css'
+
+export default function Interests() {
+    return (
+        <div className="interests-container">
+            <h1 className="interests-title">Interests</h1>
+            <p className="interests-content">AI enthusiast. Music producer. Avid reader. Tech aficionado. Foodie. Entrepreneur. Travel enthusiast. Pop culture expert. Coffee lover.
+            </p>
+        </div>
+    )
+}
