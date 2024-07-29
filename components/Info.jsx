5ab82c8965db9b68000383132e5e10ba452426c8
Levi Adissi <leviadissi@gmail.com>
2024-07-28 17:35:22 -0700

    Add files via upload

--- components/Info.jsx
+++ components/Info.jsx
@@ -1,10 +1,11 @@
 // import React from "react"
 import '../src/style.css'
+import linkedinPic from "public/00_linkedinLevi.jpeg"
 
 export default function Info() {
     return(
         <div className="info-container">
-            <img className="img-profile" src="public/00_linkedinLevi.jpeg" />
+            <img className="img-profile" src={linkedinPic} />
             <h1 className="info-name">Levi Adissi</h1>
             <h2 className="info-title">Product Manager</h2>
             <h3 className="info-site"><a href="https://ladissi.github.io/">ladissi.github.io</a></h3>
